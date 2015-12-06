5fe8aafb73b30df7ecbecc43c0d08498dd956776
patrick brisbin <pbrisbin@gmail.com>
2015-12-06 10:41:29 -0500

    Use exec in start script
    Ensures the CouchPotato process is PID 1 for correct signal handling

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ mkdir -p /data
 cd /CouchPotatoServer
 touch /config/CouchPotato.cfg
 
-/usr/bin/python /CouchPotatoServer/CouchPotato.py --data_dir /data/ --config_file=/config/CouchPotato.cfg --console_log
+exec /usr/bin/python /CouchPotatoServer/CouchPotato.py --data_dir /data/ --config_file=/config/CouchPotato.cfg --console_log
